f816904707dbe5d4adfc8565a9c3917b4b6dce79
Addo-Freda <fredaaddo908@gmail.com>
2023-02-16 11:51:30 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unixstd.h>
 /**
- * main - A program that prints a line with standard error
- *
+ * main - A program that prints a line of code to standard output
  * Return: 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }

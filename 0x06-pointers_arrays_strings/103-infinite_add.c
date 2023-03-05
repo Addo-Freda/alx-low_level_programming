3d553540e446b9d35ec025b1e11527be39e2e98b
Addo-Freda <fredaaddo908@gmail.com>
2023-03-05 04:57:00 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,46 +1,78 @@
 #include "main.h"
-#include <string.h>
+
 /**
- * infinite_add - A function that adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: the buffer that the function will use to store the result
- * @size_r: the buffer size
- * Return: A pointer to the result
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-       	int max_len = (len1 > len2) ? len1 : len2;
-	int carry = 0;
-        int i = len1 - 1;
-        int j = len2 - 1;
-        int k = max_len;
-	int d1 = (i >= 0) ? n1[i] - '0' : 0;
-	int d2 = (j >= 0) ? n2[j] - '0' : 0;
-	int sum = d1 + d2 + carry;
-	
-	if (max_len + 1 > size_r)
-	{
-	return (0);
-	}
-	while (i >= 0 || j >= 0)
-	{
-	carry = sum / 10;
-	r[k] = sum % 10 + '0';
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
 	i--;
 	j--;
-	k--;
-	}
-	if (carry)
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-	r[k] = carry + '0';
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	else
-	{
-	k++;
-	}
-	r[max_len + 1] = '\0';
-	return (&r[k]);
-}	
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
